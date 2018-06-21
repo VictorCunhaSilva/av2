5270e3a12b8b20dc3b5141c2efe649ff63d279d9
victor cunha <vcunha1503@gmail.com>
2018-06-21 03:04:49 +0000

    login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,20 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'users/new'
+
   resources :categoria
     get "home" => "home#home"
     get "aula" => "aula#aula"
     get "login" => "login#login"
     get "upload" => "upload#upload"
-    root :to => "login#login"
+    #root :to => "login#login"
+    get 'sessions/new'
+    resources :users
+    get    'sign_in'   => 'sessions#new'
+    post   'sign_in'   => 'sessions#create'
+    delete 'sign_out'  => 'sessions#destroy'
+     root 'sessions#new'
+     
 end
+
